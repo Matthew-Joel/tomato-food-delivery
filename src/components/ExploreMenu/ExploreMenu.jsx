2cd20144e50cb54c424f77ebf929de52f5407fe3
Matthew-Joel <joelmathew826@gmail.com>
2024-06-14 23:25:26 +0100

    explore styling

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,8 +1,22 @@
 import "./ExploreMenu.css"
-
+import { menu_list } from "../../assets/assets"
 const ExploreMenu = () => {
   return (
-    <div>ExploreMenu</div>
+    <div className="explore-menu" id="explore-menu">
+        <h1>Explore our menu</h1>
+        <p className="explore-menu-text">Choose from a diverse menu featuring a delectable array of dishes.Our mission is to satisfy your cravings and elevate your dining experience.one delicious meal at a time</p>
+        <div className="explore-menu-list">
+            {menu_list.map((item,index)=>{
+                return (
+                    <div key={index} className="explore-menu-list-item">
+                        <img src={item.menu_image} alt="menu" />
+                        <p>{item.menu_name}</p>
+
+                    </div>
+                )
+            })}
+        </div>
+    </div>
   )
 }
 
