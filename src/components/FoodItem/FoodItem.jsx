d93d0f653f7960e649294e0cf3ce4c876670f7a4
Matthew-Joel <joelmathew826@gmail.com>
2024-06-15 00:30:24 +0100

    food-card

---
+++ src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,22 @@
+import { assets } from "../../assets/assets"
+import "./FoodItem.css"
+
+const FoodItem = ({id,name,price,description,image}) => {
+  return (
+    <div className="food-item">
+        <div className="food-item-img-container">
+            <img src={image} alt="image" />
+        </div>
+        <div className="food-item-info">
+            <div className="food-item-name">
+                <p>{name}</p>
+                <img src={assets.rating_starts} alt="rating" />
+            </div>
+            <p className="food-item-desc">{description}</p>
+            <p className="food-item-price">${price}</p>
+        </div>
+    </div>
+  )
+}
+
+export default FoodItem
