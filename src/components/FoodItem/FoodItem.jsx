97435714495ce434aff4659bca6e1321128e9561
Matthew-Joel <joelmathew826@gmail.com>
2024-06-15 02:12:43 +0100

    fixes

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react"
 import { assets } from "../../assets/assets"
 import "./FoodItem.css"
 
 const FoodItem = ({id,name,price,description,image}) => {
+
+    const [itemCount,setItemCount] = useState(0)
   return (
     <div className="food-item">
         <div className="food-item-img-container">
-            <img src={image} alt="image" />
+            <img src={image} alt="image" className="food-item-image"/>
+            {!itemCount
+            
+            }
         </div>
         <div className="food-item-info">
-            <div className="food-item-name">
+            <div className="food-item-name-rating">
                 <p>{name}</p>
                 <img src={assets.rating_starts} alt="rating" />
             </div>
