c485f1bf1594ef92c4e66b4281424fc0f0fca14e
Matthew-Joel <joelmathew826@gmail.com>
2024-06-15 07:48:01 +0100

    fooditem page

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,28 +1,33 @@
-import { useState } from "react"
-import { assets } from "../../assets/assets"
-import "./FoodItem.css"
+import { useState } from "react";
+import { assets } from "../../assets/assets";
+import "./FoodItem.css";
 
-const FoodItem = ({id,name,price,description,image}) => {
-
-    const [itemCount,setItemCount] = useState(0)
+const FoodItem = ({ id, name, price, description, image }) => {
+  const [itemCount, setItemCount] = useState(0);
   return (
     <div className="food-item">
-        <div className="food-item-img-container">
-            <img src={image} alt="image" className="food-item-image"/>
-            {!itemCount
-            
-            }
+      <div className="food-item-img-container">
+        <img className="food-item-image" src={image} alt="image" />
+        {!itemCount ? (
+          <img
+            className="add"
+            onClick={() => setItemCount((prev) => prev + 1)}
+            src={assets.add_icon_white}
+          />
+        ) : (
+          <div className="food-item-counter"></div>
+        )}
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="rating" />
         </div>
-        <div className="food-item-info">
-            <div className="food-item-name-rating">
-                <p>{name}</p>
-                <img src={assets.rating_starts} alt="rating" />
-            </div>
-            <p className="food-item-desc">{description}</p>
-            <p className="food-item-price">${price}</p>
-        </div>
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">${price}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodItem
+export default FoodItem;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.css"
 
 export const Header = () => {
+  
   return (
     <div className="header">
         <div className="header-contents">
